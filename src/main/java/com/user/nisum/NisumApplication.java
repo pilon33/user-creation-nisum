cecae1d470ca888884eda601c9bcd2cbf7c74a3e
josefranval <josefranval88@gmail.com>
2025-07-28 11:02:46 -0300

    config: config initial

---
+++ src/main/java/com/user/nisum/NisumApplication.java
@@ -0,0 +1,13 @@
+package com.user.nisum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.user.nisum.NisumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/nisum/NisumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.nisum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
